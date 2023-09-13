bd2daf0216db269ff15b50f5c6a890cdd8a87aaf
GAYEON JUNG <113704015+Gayeon6423@users.noreply.github.com>
2023-09-13 23:53:08 +0900

    Time: 1772 ms (86.53%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -1,11 +1,11 @@
-# # where 사용 version
-# SELECT product_id, year as first_year, quantity, price
-# FROM Sales
-# WHERE (product_id, year) IN 
-# (select product_id, MIN(year) from Sales GROUP BY product_id)
+# where 사용 version
+SELECT product_id, year as first_year, quantity, price
+FROM Sales
+WHERE (product_id, year) IN 
+(select product_id, MIN(year) from Sales GROUP BY product_id)
 
-# subquery 사용 version
-SELECT s.product_id, s.year as first_year, quantity, price
-FROM sales as s
-JOIN (select product_id, MIN(year) AS min_year FROM Sales GROUP BY product_id) subq
-ON s.product_id = subq.product_id AND s.year = subq.min_year
+# # subquery 사용 version
+# SELECT s.product_id, s.year as first_year, quantity, price
+# FROM sales as s
+# JOIN (select product_id, MIN(year) AS min_year FROM Sales GROUP BY product_id) subq
+# ON s.product_id = subq.product_id AND s.year = subq.min_year
