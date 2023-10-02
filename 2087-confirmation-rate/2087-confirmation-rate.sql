ac8669f39477b8dd623d50caef45f87a25f32eca
GAYEON JUNG <113704015+Gayeon6423@users.noreply.github.com>
2023-10-02 14:45:50 +0900

    Time: 1242 ms (88.01%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -2,18 +2,17 @@
 -- confrimation rate = 0 if confirmation 메시지 수 = 0
 -- 2자릿수에서 반올림
 
-SELECT S.user_id,
-                  CASE WHEN COUNT(c.user_id) = 0 THEN 0
-                       ELSE ROUND(SUM(CASE WHEN C.action = 'confirmed' THEN 1 ELSE 0 END)                   / COUNT(C.user_id),2) END as confirmation_rate
-FROM Signups as S  
-LEFT OUTER JOIN Confirmations as C ON S.user_id = C.user_id
-GROUP BY S.user_id
-
-# SELECT S.user_id, 
-#        CASE 
-#            WHEN COUNT(C.user_id) = 0 THEN 0
-#            ELSE ROUND((SUM(CASE WHEN C.action = 'confirmed' THEN 1 ELSE 0 END) / COUNT(C.user_id)) * 100, 2)
-#        END as confirmation_rate
+-- version 1
+# SELECT S.user_id,
+#                   CASE WHEN COUNT(c.user_id) = 0 THEN 0
+#                        ELSE ROUND(SUM(CASE WHEN C.action = 'confirmed' THEN 1 ELSE 0 END)                   / COUNT(C.user_id),2) END as confirmation_rate
 # FROM Signups as S  
 # LEFT OUTER JOIN Confirmations as C ON S.user_id = C.user_id
 # GROUP BY S.user_id
+
+-- version 2
+SELECT S.user_id, ROUND(AVG(if(c.action = 'confirmed',1,0)),2) as confirmation_rate
+FROM Signups as S
+LEFT OUTER JOIN Confirmations as C
+ON S.user_id = C.user_id
+GROUP BY user_id
