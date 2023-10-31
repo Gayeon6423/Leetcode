083e9cbea78d5f22394cda4852659aa9379f9e10
GAYEON JUNG <113704015+Gayeon6423@users.noreply.github.com>
2023-10-31 14:23:25 +0900

    Time: 1219 ms (31.57%), Space: 0B (100%) - LeetHub

--- 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -8,11 +8,20 @@
 # FROM Employee
 # GROUP BY employee_id;
 
+# SELECT employee_id, department_id
+# FROM Employee
+# GROUP BY employee_id
+# HAVING COUNT(employee_id) = 1
+# UNION
+# SELECT employee_id, department_id
+# FROM Employee
+# WHERE primary_flag = 'Y'
+
 SELECT employee_id, department_id
 FROM Employee
+WHERE primary_flag = 'Y' OR
+      employee_id IN(SELECT employee_id
+                      FROM Employee
+                      GROUP BY employee_id
+                      HAVING COUNT(employee_id) = 1)
 GROUP BY employee_id
-HAVING COUNT(employee_id) = 1
-UNION
-SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag = 'Y'
